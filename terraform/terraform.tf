ef1fde41de21b1b062355d6d764f60f6e3970d61
Stephane Jourdan <fasten@fastmail.fm>
2015-05-05 23:16:28 +0200

    cleaner terraform file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,7 +6,7 @@ provider "digitalocean" {
 # Create a new SSH key
 resource "digitalocean_ssh_key" "default" {
     name = "DO SSH Key"
-    public_key = "${file("~/.ssh/digitalocean.pub")}"
+    public_key = "${file("${var.ssh_key_file}.pub")}"
 }
 
 # Create a new vault droplet
@@ -38,31 +38,3 @@ EOF
 output "core-1.ipv4_address" {
 	value = "${digitalocean_droplet.coreos-1.ipv4_address}"
 }
-
-/*resource "digitalocean_droplet" "coreos-2" {
-    image = "coreos-stable"
-    name = "core-2"
-    region = "ams3"
-    size = "512mb"
-    ssh_keys = ["${digitalocean_ssh_key.default.id}"]
-    private_networking = true
-    user_data = <<EOF
-#cloud-config
-coreos:
-  etcd:
-    discovery: https://discovery.etcd.io/4911adcd842ee96f5575a602c9926514
-    addr: $private_ipv4:4001
-    peer-addr: $private_ipv4:7001
-  fleet:
-    public-ip: $private_ipv4
-  units:
-    - name: etcd.service
-      command: start
-    - name: fleet.service
-      command: start
-EOF
-}
-
-output "core-2.ipv4_address" {
-	value = "${digitalocean_droplet.coreos-2.ipv4_address}"
-}*/
